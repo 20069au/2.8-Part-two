b30b772822993122d3cc2dbdfbec4ebae5acac43
20069au <125605125+20069au@users.noreply.github.com>
2023-05-16 00:09:22 +0000

    I have my first vr button working

---
+++ script.js
@@ -0,0 +1,10 @@
+/** function createButtons (){
+      //enterButton = src(/images/vrworld.png);
+      enterButton = new Sprite (width / 2, height / 2, 300, 100, 'n');
+      enterButton.color = color ('white');
+      enterButton.stroke = color ('black');
+      enterButton.textSize = 50;
+      enterButton.text = ("You Lost!");
+};
+
+createButtons ();*/
