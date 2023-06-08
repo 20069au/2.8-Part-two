3b4f5dea86483db1dbbd35cfba89719c6ec0190d
20069au <125605125+20069au@users.noreply.github.com>
2023-06-08 23:01:33 +0000

    Fixed buttons

--- script.js
+++ script.js
@@ -1,10 +1,110 @@
-/** function createButtons (){
-      //enterButton = src(/images/vrworld.png);
-      enterButton = new Sprite (width / 2, height / 2, 300, 100, 'n');
-      enterButton.color = color ('white');
-      enterButton.stroke = color ('black');
-      enterButton.textSize = 50;
-      enterButton.text = ("You Lost!");
-};
+var showbanjoPop = true;
+function banjoPopup() {
+  if (showbanjoPop) {
+    banjoPop.setAttribute("visible", true);
+    showbanjoPop = false;
+    console.log('banjo worked');
+  }
+  else {
+    banjoPop.setAttribute("visible", false);
+    showbanjoPop = true;
+  }
+}
 
-createButtons ();*/
+
+var showCelloPop = true;
+function celloPopup() {
+  if (showCelloPop) {
+    celloPop.setAttribute("visible", true);
+    showCelloPop = false;
+    console.log('cello worked');
+  }
+  else {
+    celloPop.setAttribute("visible", false);
+    showCelloPop = true;
+  }
+}
+
+
+var showTrumpetPop = true;
+function trumpetPopup() {
+  if (showTrumpetPop) {
+    trumpetPop.setAttribute("visible", true);
+    showTrumpetPop = false;
+    console.log('trumpet worked');
+  }
+  else {
+    trumpetPop.setAttribute("visible", false);
+    showTrumpetPop = true;
+  }
+}
+
+
+var showSaxPop = true;
+function saxPopup() {
+  if (showSaxPop) {
+    saxPop.setAttribute("visible", true);
+    showSaxPop = false;
+    console.log('saxaphone worked');
+  }
+  else {
+    saxPop.setAttribute("visible", false);
+    showSaxPop = true;
+  }
+}
+
+
+var showPianoPop = true;
+function pianoPopup() {
+  if (showPianoPop) {
+    pianoPop.setAttribute("visible", true);
+    showPianoPop = false;
+    console.log('piano worked');
+  }
+  else {
+    pianoPop.setAttribute("visible", false);
+    showPianoPop = true;
+  }
+}
+
+
+var showGuitarPop = true;
+function guitarPopup() {
+  if (showGuitarPop) {
+    guitarPop.setAttribute("visible", true);
+    showGuitarPop = false;
+    console.log('guitar worked');
+  }
+  else {
+    guitarPop.setAttribute("visible", false);
+    showGuitarPop = true;
+  }
+}
+
+
+var showFlutePop = true;
+function flutePopup() {
+  if (showFlutePop) {
+    flutePop.setAttribute("visible", true);
+    showFlutePop = false;
+    console.log('flute worked');
+  }
+  else {
+    flutePop.setAttribute("visible", false);
+    showFlutePop = true;
+  }
+}
+
+
+var showDrumsPop = true;
+function drumsPopup() {
+  if (showDrumsPop) {
+    drumsPop.setAttribute("visible", true);
+    showDrumsPop = false;
+    console.log('drums worked');
+  }
+  else {
+    drumsPop.setAttribute("visible", false);
+    showDrumsPop = true;
+  }
+}
